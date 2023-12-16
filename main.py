b30a5516732aa9c86720945b10123a21949e7d7e
dbuawork <dbuawork@gmail.com>
2023-12-16 16:04:10 +0200

    second-edit

--- main.py
+++ main.py
@@ -13,4 +13,12 @@ elif n3 >= n1 and n3 >= n1:
 else:
     print(f"equals")
 
-
+# # Знаходимо найменше із трьох
+if n1 <= n2 and n1 <= n3:
+    print(f"Найменше число: {n1}")
+elif n2 <= n1 and n2 <= n3:
+    print(f"Найменше число: {n2}")
+elif n3 <= n1 and n3 <= n1:
+    print(f"Найменше число: {n3}")
+else:
+    print(f"equals")
